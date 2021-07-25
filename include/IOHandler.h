231f8a5e74791812dfc2d28869e94f8b28ea5b58
sparrow.wsl <foxintango@hotmail.com>
2021-07-25 22:19:32 +0800

    null

--- include/IOHandler.h
+++ include/IOHandler.h
@@ -4,22 +4,24 @@
 
 EXTERN_C_BEGIN
 namespace foxintango {
-    class IOEvent;
-    class IOSession;
-    class foxintangoAPI IOEventHandler {
-        public:
-            IOEventHandler(){}
-            virtual ~IOEventHandler(){};
-        public:
-            virtual int handleEvent(const IOEvent* event) = 0;
-    };
-    class foxintangoAPI IOSessionHandler {
-        public:
-            IOSessionHandler();
-            virtual ~IOSessionHandler() {};
-        public:
-            virtual int handleSession(const IOSession* session) = 0;
-    };
+class IOEvent;
+class IOSession;
+class foxintangoAPI IOEventHandler {
+public:
+    IOEventHandler(){}
+    virtual ~IOEventHandler(){};
+public:
+    virtual int handleEvent(const IOEvent* event) = 0;
+};
+    
+class foxintangoAPI IOSessionHandler {
+public:
+    IOSessionHandler();
+    virtual ~IOSessionHandler() {};
+public:
+    virtual int handleSession(const IOSession* session) = 0;
+};
+
 }
 EXTERN_C_END
 #endif
