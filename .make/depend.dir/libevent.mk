731a725cd11f95e0b8b057836518dcaaabd5a95f
李诗军 <lidali@AllinoneBook.local>
2025-10-14 15:57:36 +0800

    2025年10月14日 星期二 15时57分36秒 CST

--- .make/depend.dir/libevent.mk
+++ .make/depend.dir/libevent.mk
@@ -1,12 +1,12 @@
 DEPEND_TARGETS += libevent.recursive
 ECHO_TARGETS += libevent.echo
 libevent.recursive:
-	@echo SUPER_MAKE_DIR=/volumes/llama/home/alpine/libraries/libioevent/               >> /volumes/llama/home/alpine/libraries/libevent/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/volumes/llama/home/alpine/libraries/libioevent/.make >> /volumes/llama/home/alpine/libraries/libevent/.make/super
-	cd /volumes/llama/home/alpine/libraries/libevent/ && make recursive && make install
-	-rm /volumes/llama/home/alpine/libraries/libevent/.make/super
+	@echo SUPER_MAKE_DIR=/Users/lidali/alpine/libraries/libioevent/               >> /Users/lidali/alpine/libraries/libevent/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/Users/lidali/alpine/libraries/libioevent/.make >> /Users/lidali/alpine/libraries/libevent/.make/super
+	cd /Users/lidali/alpine/libraries/libevent/ && /Applications/Xcode.app/Contents/Developer/usr/bin/make recursive && /Applications/Xcode.app/Contents/Developer/usr/bin/make install
+	-rm /Users/lidali/alpine/libraries/libevent/.make/super
 libevent.echo:
-	@echo SUPER_MAKE_DIR=/volumes/llama/home/alpine/libraries/libioevent/               >> /volumes/llama/home/alpine/libraries/libevent/.make/super
-	@echo SUPER_MAKE_CONFIG_DIR=/volumes/llama/home/alpine/libraries/libioevent/.make >> /volumes/llama/home/alpine/libraries/libevent/.make/super
-	cd /volumes/llama/home/alpine/libraries/libevent/ && make echo
-	-rm /volumes/llama/home/alpine/libraries/libevent/.make/super
+	@echo SUPER_MAKE_DIR=/Users/lidali/alpine/libraries/libioevent/               >> /Users/lidali/alpine/libraries/libevent/.make/super
+	@echo SUPER_MAKE_CONFIG_DIR=/Users/lidali/alpine/libraries/libioevent/.make >> /Users/lidali/alpine/libraries/libevent/.make/super
+	cd /Users/lidali/alpine/libraries/libevent/ && /Applications/Xcode.app/Contents/Developer/usr/bin/make echo
+	-rm /Users/lidali/alpine/libraries/libevent/.make/super
